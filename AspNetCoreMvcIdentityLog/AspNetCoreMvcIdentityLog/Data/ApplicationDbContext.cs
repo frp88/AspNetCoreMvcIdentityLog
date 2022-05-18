1ae0e7c39afce9791042c713ac7e740b71bf93fd
Fernando Roberto Proença <fernandorroberto@gmail.com>
2022-05-18 15:21:33 -0300

    Se aplicado, adicionara o controle de acesso com o Identity e o Log de auditoria.

---
+++ AspNetCoreMvcIdentityLog/AspNetCoreMvcIdentityLog/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AspNetCoreMvcIdentityLog.Models;
+
+namespace AspNetCoreMvcIdentityLog.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Produto> Produto { get; set; }
+
+        public DbSet<LogAuditoria> LogAuditoria { get; set; }
+    }
+}
---
+++ AspNetCoreMvcIdentityLog/AspNetCoreMvcIdentityLog/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspNetCoreMvcIdentityLog.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        [DisplayName("Código")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [DisplayName("Nome")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Nome { get; set; }
+
+    }
+}
